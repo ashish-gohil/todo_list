5d2a19c6e5af50bdf4437396c3935c99313b0bb2
Ashish Gohil <ashishgohil148@gmail.com>
2023-02-02 14:45:28 +0530

    initial commit: home and work page add with EJS

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+let ejs = require('ejs');
+app.set('view engine', 'ejs');
+
+let items = [];
+let work_items = [];
+
+app.get('/', (req, res) => {
+    const day = new Date();
+    const d = day.toLocaleString('en-IN', { weekday: 'short', month: '2-digit', year: 'numeric', day: 'numeric' })
+    const name = '';
+    res.render('index', { head: d, items: items, name: 'home' });
+});
+
+app.get('/work', (req, res) => {
+    res.render('index', { head: 'Work', items: work_items, name: 'work' })
+});
+
+app.post('/', (req, res) => {
+    if (req.body.name == 'work') {
+        work_items.push(req.body.add);
+        res.redirect('/work');
+    } else {
+        items.push(req.body.add);
+        res.redirect('/');
+    }
+
+});
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('App is runnnig at port 3000!')
+})
